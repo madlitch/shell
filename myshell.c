c7919e5ecdb4e85b78da5c67bd888b2573ef2594
Daanyaal Tahir <tahir.daanyaal@gmail.com>
2023-02-19 12:15:16 -0500

    add changes

--- myshell.c
+++ myshell.c
@@ -8,50 +8,48 @@ Group 10
 #include <dirent.h>
 #include <sys/types.h>
 #include <string.h>
+
 #include "utility.h"
 #include "myshell.h"
 
-// Put macros or constants here using #define
-#define BUFFER_LEN 1024
-#define MAX_TOKENS 10
+#define MAXIMUM_TOKENS 10
+#define BUFFER_LENGTH 1024
 
 int main(int argc, char *argv[])
 {
-    // Input buffer and and commands
-	char environ[2][BUFFER_LEN] = {0};
-	char pwd[BUFFER_LEN] = {0};
-	char myshell[BUFFER_LEN] = {0};
-    char buffer[BUFFER_LEN] = {0};
-    char command[BUFFER_LEN] = {0};
-    char tokens[MAX_TOKENS][BUFFER_LEN] = {0};
+    // Initialize the variables
+	char environ[2][BUFFER_LENGTH], pwd[BUFFER_LENGTH], shell[BUFFER_LENGTH], buffer[BUFFER_LENGTH], command[BUFFER_LENGTH], tokens[MAXIMUM_TOKENS][BUFFER_LENGTH] = {0};
 	int token_count = 0;
+	FILE* fd; // Declare fd for batchfile
 
-	FILE* fp = NULL;
-    // Parse the commands provided using argc and argv
+	// Check to see if user has provided a batchfile
     if (argc > 1){
-		fp = fopen(argv[1],"r");
-		if (fp == NULL){
-			printf("Error opening batch file...\n");
-			return EXIT_FAILURE;
+		// If batchfile, then open the file for reading
+		fd = fopen(argv[1],"r");
+		if (fd == NULL){
+			perror("There was an error opening the specified file");
+			exit(1);
 		}
 	}
 	else {
-		fp = stdin;
+		fd = stdin;
 	}
 	
-    // Perform an infinite loop getting command input from users
 	
-	printf("\n-------------------------------------------------------\n");
-	printf("\nWelcome to the shell!, type help to discover more commands\n\n");
+	printf("\n*************************************************************\n\n");
+	printf("\n                 **** Welcome to myshell ****                  \n");
+	printf("\n*************************************************************\n\n\n\n");
 	get_currentDir(pwd);
-	get_currentDir(myshell);
-	strcpy(environ[0], "PWD: ");
-	strcpy(environ[1], "MYSHELL: ");
+	get_currentDir(shell);
+
 	strcat(environ[0], pwd);
-	strcat(environ[1], myshell);
+	strcat(environ[1], shell);
 	
 	printf("%s> ", pwd);
-    while (fgets(buffer, BUFFER_LEN, fp) != NULL){
+
+	// Continuously loop to get user input from terminal or file
+	// and end loop when end of fille or shell is exited 
+    while (fgets(buffer, BUFFER_LENGTH, fd) != NULL){
         // Perform string tokenization to get the command and argument
 	    int index = 0;
 		while (buffer[index] != '\n'){
@@ -62,39 +60,29 @@ int main(int argc, char *argv[])
 		token_count = string_tokenizer(buffer, tokens);
 		strcpy(command, tokens[0]);
 		
-        // Check the command and execute the operations for each command
-        // cd command -- change the current directory
+        // 'cd' command for changing diretory. 
         if (strcmp(command, "cd") == 0){
-            // your code here
-			change_dir(pwd, tokens[1]);
-			strcpy(environ[0], "PWD: ");
+			change_dir(pwd, tokens[1]); // update the pwd
 			strcat(environ[0], pwd);
         }
-
-        // other commands here...
 		
-		// clear screen
+		// 'clr' command for clearing the screen. 
 		else if (strcmp(command, "clr") == 0){
 			clear_screen();
 		}
 
-		// display help
-		else if (strcmp(command, "help") == 0){
-			 display_help();
-		}	
-		
-		// pause the shell
-		else if (strcmp(command, "pause") == 0){
-			pause_shell();
-		}	
-		
-		// lists the contents of the specified directory
+		// 'dir' command to list the contens of directory
 		else if (strcmp(command, "dir") == 0){
 			display_dir(tokens[1]);
 			printf("\n");
 		}
-		
-		// run the echo command
+
+		// 'environ' command to list the environment variables
+		else if (strcmp(command, "environ") == 0){
+			display_environs(environ);
+		}
+
+		// 'echo' command to display a comment
 		else if (strcmp(command, "echo") == 0){
 			printf("%s> ", pwd);
 			for (int i = 1; i < token_count; i++){
@@ -102,16 +90,21 @@ int main(int argc, char *argv[])
 			}
 			printf("\n");
 		}
+
+		// 'help' command to display the user manual
+		else if (strcmp(command, "help") == 0){
+			 display_help();
+		}	
 		
-		// display environment variables
-		else if (strcmp(command, "environ") == 0){
-			display_environs(environ);
-		}
+		// 'pause' command to pause the operation of the shell
+		else if (strcmp(command, "pause") == 0){
+			pause_shell();
+		}	
 		
-        // quit command -- exit the shell
-        else if (strcmp(tokens[0], "quit") == 0 || strcmp(tokens[0], "exit") == 0){
+        // 'quit' command to quit the shell
+        else if (strcmp(tokens[0], "quit") == 0){
             printf("Bye!\n");
-			return EXIT_SUCCESS;
+			exit(0);
         }
 
         // Unsupported command
@@ -120,13 +113,15 @@ int main(int argc, char *argv[])
             printf("Unsupported command, use help to display the manual\n");
         }
 		
+		// clear the buffer, command and token variables
 		memset(buffer, 0, sizeof buffer);
 		memset(command, 0, sizeof command);
-		for (int i = 0; i < MAX_TOKENS; i++){
+		for (int i = 0; i < MAXIMUM_TOKENS; i++){
 			memset(tokens[i], 0, sizeof tokens[i]);
 		}
 		
+		// print the present working directory
 		printf("%s> ", pwd);
     }
-    return EXIT_SUCCESS;
+    exit(0);
 }
